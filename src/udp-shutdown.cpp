0ca8fed76a4b25f032553f8c11fd5e12b218ad94
95lux <jlx@235media.com>
2023-04-14 12:50:13 +0200

    pretty up console output and add needed .dlls to /bin

--- src/udp-shutdown.cpp
+++ src/udp-shutdown.cpp
@@ -51,6 +51,8 @@ int main() {
 
     // configure config
     Config config;
+    const char *shutdown_cmd = config.shutdown_cmd;
+    int port = config.port;
 
     // setup socket
     SOCKET s;
@@ -61,35 +63,36 @@ int main() {
 
     slen = sizeof(si_other);
 
+    puts("235Media udp-shutdown\n\n");
+
     // Initialise winsock
-    printf("\nInitialising Winsock...");
+    printf("[ WRN ] Initialising Winsock...\n");
     if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
-        printf("Failed. Error Code : %d", WSAGetLastError());
+        printf("[ ERR ] Failed. Error Code : %d", WSAGetLastError());
         exit(EXIT_FAILURE);
     }
-    printf("Initialised.\n");
+    printf("[ OK ] Initialised.\n");
 
     // Create a socket
     if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET) {
-        printf("Could not create socket : %d", WSAGetLastError());
+        printf("[ ERR ] Could not create socket : %d", WSAGetLastError());
     }
-    printf("Socket created.\n");
+    printf("[ OK ] Socket created.\n");
 
     // Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(config.port);
-
+    server.sin_port = htons(port);
+    printf("[ WRN ] Binding...\n");
     // Bind
     if (bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR) {
-        printf("Bind failed with error code : %d", WSAGetLastError());
+        printf("[ ERR ] Bind failed with error code : %d", WSAGetLastError());
         exit(EXIT_FAILURE);
     }
-    puts("Bind done");
-
+    printf("[ OK ] Bind done\n");
+    printf("[ WRN ] Listening for shutdown command: \"%s\" on port :%i\n", shutdown_cmd, port);
     // keep listening for data
     while (1) {
-        printf("Waiting for data...");
         fflush(stdout);
 
         // clear the buffer by filling null, it might have previously received data
@@ -97,20 +100,18 @@ int main() {
 
         // try to receive some data, this is a blocking call
         if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *)&si_other, &slen)) == SOCKET_ERROR) {
-            printf("recvfrom() failed with error code : %d", WSAGetLastError());
+            printf("[ ERR ] recvfrom() failed with error code : %d", WSAGetLastError());
             exit(EXIT_FAILURE);
         }
 
         // print details of the client/peer and the data received
-        printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-        printf("Data: %s\n", buf);
-        if (strcmp(buf, config.shutdown_cmd) == 0) {
+        printf("[ OK ] Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+        printf("[ OK ] Data: %s\n", buf);
+
+        // exec shutdown if received data equals shutdown_cmd
+
+        if (strcmp(buf, shutdown_cmd) == 0) {
             systemShutdown();
-        }
-        // now reply the client with the same data
-        if (sendto(s, buf, recv_len, 0, (struct sockaddr *)&si_other, slen) == SOCKET_ERROR) {
-            printf("sendto() failed with error code : %d", WSAGetLastError());
-            exit(EXIT_FAILURE);
         }
     }
     closesocket(s);
